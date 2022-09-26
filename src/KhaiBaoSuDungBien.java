07790a0f91696eb5e3fde594fa38a74890a311a7
ChuMinh28 <quangminh2872000@gmail.com>
2022-09-26 16:17:33 +0700

    Thuc Hanh 2

---
+++ src/KhaiBaoSuDungBien.java
@@ -0,0 +1,14 @@
+public class KhaiBaoSuDungBien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 28.7f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Bắc Ninh";
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
